9d34e0b21db67760daedcf618e8d09bbb8cd908b
Blplay13 <95048551+Blplay13@users.noreply.github.com>
2022-01-06 20:31:42 +0200

    Update Program.cs

--- FridayNightRandomReturn/Program.cs
+++ FridayNightRandomReturn/Program.cs
@@ -10,6 +10,31 @@ namespace FridayNightRandomReturn
     {
         static void Main(string[] args)
         {
+            string randomFood, randomDrink, randomMovie;
+            string[] foods = { "chicken wings", "pizza", "sushi", "salad", "wraps" };
+            string[] drinks = { "apple juice", "Dr.Pepper", "sparkling water", "Sprite", "Coffee" };
+            string[] movies = { "Home Alone", "Star Wars", "Avengers", "Lethal Weapon", "Twilight" };
+
+            randomFood = PickRandomElementFromArray(foods);
+            randomDrink = PickRandomElementFromArray(drinks);
+            randomMovie = PickRandomElementFromArray(movies);
+
+            Console.WriteLine($"Tonight you will have {randomFood} with {randomDrink} and watch {randomMovie}.");
+
+        }
+
+        private static int GenerateRandomIndex(string[] someArray)
+        {
+            Random rnd = new Random();
+            int randomIndex = rnd.Next(0, someArray.Length);
+
+            return randomIndex;
+        }
+
+        private static string PickRandomElementFromArray(string[] someArray)
+        {
+            string randomElement = someArray[GenerateRandomIndex(someArray)];
+            return randomElement;
         }
     }
 }
