98e2ea12c128d46709cca88cd747ca24172f5a1d
Blplay13 <95048551+Blplay13@users.noreply.github.com>
2022-01-06 20:33:28 +0200

    Update Program.cs

--- Chuck Norris/Program.cs
+++ Chuck Norris/Program.cs
@@ -10,6 +10,26 @@ namespace Chuck_Norris
     {
         static void Main(string[] args)
         {
+            string randomJoke;
+            randomJoke = GetRandomFromFile("chuck");
+            Console.WriteLine($"{randomJoke}.");
+        }
+
+        private static int GenerateRandomIndex(string[] someArray)
+        {
+            Random rnd = new Random();
+            int randomIndex = rnd.Next(0, someArray.Length);
+
+            return randomIndex;
+        }
+
+        private static string GetRandomFromFile(string fileName)
+        {
+            string filePath = $@"C:\Users\...\Samples\chuck.txt";
+            string[] dataFromFile = File.ReadAllLines(filePath);
+            string randomElement = dataFromFile[GenerateRandomIndex(dataFromFile)];
+
+            return randomElement;
         }
     }
 }
